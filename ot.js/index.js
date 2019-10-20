1805a1675710c2e9f30734b9179c58db862e2915
kelvinko <kelvinkoko@gmail.com>
2019-10-20 16:47:13 +0900

    Remove unused file

--- ot.js/index.js
+++ ot.js/index.js
@@ -1,7 +1,6 @@
 exports.version = '0.0.15';
 
 exports.TextOperation        = require('./text-operation');
-exports.SimpleTextOperation  = require('./simple-text-operation');
 exports.Client               = require('./client');
 exports.Server               = require('./server');
 exports.Selection            = require('./selection');
