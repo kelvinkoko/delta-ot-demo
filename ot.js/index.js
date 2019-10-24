406ebffbc98d9919e5cafa810dae48a8bac35f55
kelvinko <kelvinkoko@gmail.com>
2019-10-24 18:44:28 +0900

    Remove server code

--- ot.js/index.js
+++ ot.js/index.js
@@ -4,4 +4,3 @@ exports.TextOperation        = require('./text-operation');
 exports.Client               = require('./client');
 exports.Server               = require('./server');
 exports.Selection            = require('./selection');
-exports.EditorSocketIOServer = require('./editor-socketio-server');
